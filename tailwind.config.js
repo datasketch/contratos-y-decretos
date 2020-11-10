4ee370a413932ee558fb86c9a5e4b478809bef26
David Daza <03dazal@gmail.com>
2020-11-10 12:20:52 -0500

    estructura desktop

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        charcoal: '#434f5d',
+        'red-crayola': '#F52749',
+        'tiryan-purple': '#550f3f',
+      },
+      fontFamily: {
+        lato: ['Lato', 'sans-serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
