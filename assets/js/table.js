eff19a1a6fd180d04ed7ebad14aa8f4c737190dd
David Daza <03dazal@gmail.com>
2020-12-15 07:32:28 -0500

    change searchbar style

--- assets/js/table.js
+++ assets/js/table.js
@@ -31,41 +31,20 @@ function registerListeners(table, params) {
   });
 }
 
-function createColumnsFilter(columns) {
-  columns.forEach((column) => {
-    const id = column.field.toLowerCase().replace(/\s/g, '_');
-    const checkbox = `
-      <input
-        id="${id}"
-        type="checkbox"
-        name="column"
-        class="hidden"
-        value="${column.field}"
-        ${column.visible && 'checked'}
-      >
-      <label for="${id}" class="flex items-center space-x-1 select-none cursor-pointer">
-        <span class="column-indicator"></span>
-        <span>${column.title}</span>
-      </label>
-    `;
-    columnsFilter.innerHTML += checkbox;
-  });
-}
-
 function renderTable(response) {
   const { headers, data } = response;
   const columns = headers.map((header, index) => {
     const columnDef = {
       title: header,
       field: header,
-      minWidth: 150,
+      minWidth: 300,
       formatter: 'textarea'
     };
     if (header === 'Semaforo' || header === 'Semáforo') {
       return Object.assign(columnDef, {
         formatter: function (cell) {
           const imageUrl = imageMapper[cell.getValue().toLowerCase()];
-          return `<img class="h-10 mx-auto" src="${imageUrl}">`;
+          return `<img class="h-16 mx-auto" src="${imageUrl}">`;
         },
       });
     }
@@ -77,9 +56,7 @@ function renderTable(response) {
     layout: 'fitColumns',
   };
   const table = new Tabulator(container, options);
-  const maxPages = table.getPageMax();
-  // createColumnsFilter(columns);
-  registerListeners(table, { maxPages, searchFields: headers });
+  registerListeners(table, { searchFields: headers });
 }
 
 function main() {
